345b32990bec5d65e2b3343eda041856c88acf64
Edvin Lidholm <edvin.lidholm@volvocars.com>
2024-12-09 15:36:51 +0100

    Final solution for day 5 - TypeScript

--- solutions/day05-typescript/part2.ts
+++ solutions/day05-typescript/part2.ts
@@ -13,9 +13,13 @@ function readLines(inputFilePath: string): string[] {
 
 function processInput(inputFilePath: string): number {
     const { rules, updates } = parseInput(readLines(inputFilePath));
-    const correctUpdates = updates.filter(update => isUpdateCorrect(update, rules));
+    const incorrectUpdates = updates.filter(update => !isUpdateCorrect(update, rules));
 
-    return sumMiddleElements(correctUpdates);
+    const correctedUpdates = incorrectUpdates.map(update =>
+        correctUpdate(update, rules)
+    );
+
+    return sumMiddleElements(correctedUpdates);
 }
 
 function parseInput(inputLines: string[]): { rules: Rule[], updates: Update[] } {
@@ -54,4 +58,37 @@ function getMiddle<T>(array: T[]): T {
 
 function sumMiddleElements(updates: Update[]): number {
     return updates.reduce((sum, update) => sum + parseInt(getMiddle(update), 10), 0);
+}
+
+function correctUpdate(update: Update, rules: Rule[]): Update {
+    let isCorrect = false;
+
+    while (!isCorrect) {
+        isCorrect = true;
+
+        for (let i = 0; i < rules.length; i++) {
+            const [firstPage, secondPage] = rules[i];
+            const firstPageIdx = update.indexOf(firstPage);
+            const secondPageIdx = update.indexOf(secondPage);
+
+            if (firstPageIdx === -1 || secondPageIdx === -1) {
+                continue;
+            }
+
+            if (firstPageIdx < secondPageIdx) {
+                continue;
+            }
+
+            // If the pages are out of order, swap them
+            swapElements(update, firstPageIdx, secondPageIdx);
+            isCorrect = false;
+        }
+    }
+
+    return update;
+}
+
+
+function swapElements<T>(array: T[], idxA: number, idxB: number): void {
+    [array[idxA], array[idxB]] = [array[idxB], array[idxA]];
 }
